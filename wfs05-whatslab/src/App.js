aa8f2bb72c441389225d8625813dc4108707b1bd
Ernesto Fauth <ernestofmognol@gmail.com>
2021-05-01 13:07:31 -0300

    primeira iteracao da estrutura basica da pagina

---
+++ wfs05-whatslab/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <div className="ambiente-mensagens">
+        <div className="container-dispensavel">
+          <p>PROJETO WFS05 - WHATSLAB</p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
